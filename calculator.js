7d5eba7b789991918a97ece111f606bf15885201
saadarshnaik <71344298+saadarshnaik@users.noreply.github.com>
2021-06-13 17:56:51 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,26 +1,42 @@
-var screen=document.getElementById('screen');
-var show="";
-var holder;
+var screen=document.getElementById('screen'); //get the id=screen and assign to var screen
+var show="";  // create empty string var show
+var holder;   // create string holder 
+var operator={"+":"add","-":"substract","*":"multiply","/":"devison","%":"modulo division"}; // create object of operators for comparison
 
 function myFunction(e){
-    console.log(e.target.innerHTML)
+   
+    // when click on "AC" execute this
     if (e.target.innerHTML=="AC"){
         screen.innerHTML="00";
         show="";
 
     }
+
+    // when click on "del" execute this and delete last entry
+    else if ((e.target.innerHTML=="del") && (show !=="")){
+        show=show.substr(0,show.length-1)
+        screen.innerHTML=show
+    }
    
+    // when click on "=" execute this and show answer on the screen
     else if (e.target.innerHTML=="=") {
-       var ans=eval(holder)
-       screen.innerHTML=ans
+       var ans=eval(holder);
+       screen.innerHTML=ans;
        show=screen.innerHTML;
     } 
     
+    //else addup(string concat) every click button to var show 
     else{
-        show=show.concat(e.target.innerHTML)
+        show=show.concat(e.target.innerHTML)  // concate to show recursivly
+        
+        // to remove consecutive operators one after another
+         if ((show[show.length-1] in operator ) && (show[show.length-2] in operator)) { 
+            show=show.substr(0,show.length-1)
+        }
+        
         screen.innerHTML=show
-        holder=screen.innerHTML;
-        console.log(holder)
+        holder=screen.innerHTML; // using this "holder" in eval function 
+      
        
     };
 
